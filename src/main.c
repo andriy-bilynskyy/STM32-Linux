96e591e535cad507088315efe353086e05605f53
Andrii Bilynskyi <andriy.bilynskyy@gmail.com>
2019-04-19 23:53:06 +0300

    main: Add separate print definition to file.
    Print contains timestamp, file and LOC of additional data.
    Easy to suppress print in whole file.
    Print sample:
    [00184000] src/main.c:0020 Task 1
    
    Issue: N/A

--- src/main.c
+++ src/main.c
@@ -14,9 +14,11 @@
 #include <FreeRTOS.h>
 #include <task.h>
 
+#define DBG_OUT(fmt, ...) DBG_PRINT("[%08u] %s:%04u " fmt "\n", xTaskGetTickCount(), __FILE__, __LINE__, ##__VA_ARGS__)
+
 void vTask1(void *pvParameters)
 {
-    DBG_PRINT("Task 1 start\n");
+    DBG_OUT("Task 1 start");
 
     RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
 
@@ -25,19 +27,19 @@ void vTask1(void *pvParameters)
 
     for(;;)
     {
-        DBG_PRINT("Task 1\n");
+        DBG_OUT("Task 1");
         GPIOInvBit(GPIOC, 13);
         vTaskDelay(500);
     }
 
-    DBG_PRINT("Task 1 stop\n");
+    DBG_OUT("Task 1 stop\n");
 
     vTaskDelete(NULL);
 }
 
 void vTask2(void *pvParameters)
 {
-    DBG_PRINT("Task 2 start\n");
+    DBG_OUT("Task 2 start");
 
     RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
 
@@ -46,19 +48,19 @@ void vTask2(void *pvParameters)
 
     for(;;)
     {
-        DBG_PRINT("Task 2\n");
+        DBG_OUT("Task 2");
         GPIOInvBit(GPIOB, 12);
         vTaskDelay(100);
     }
 
-    DBG_PRINT("Task 2 stop\n");
+    DBG_OUT("Task 2 stop");
 
     vTaskDelete(NULL);
 }
 
 int main(void)
 {
-    DBG_PRINT("main start\n");
+    DBG_OUT("main start");
     (void)xTaskCreate(vTask1, "Task1", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
     (void)xTaskCreate(vTask2, "Task2", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 
